cd41392ddf9c1a61eb54ed0062a23fb6ac4b978d
Enzooohuang <yuhaohuangenzo@gmail.com>
2025-06-30 16:02:18 -0400

    Initial Commit

---
+++ bugfinderpet/bugfinderpetApp.swift
@@ -0,0 +1,17 @@
+//
+//  bugfinderpetApp.swift
+//  bugfinderpet
+//
+//  Created by Yuhao Huang on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct bugfinderpetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bugfinderpetTests/bugfinderpetTests.swift
@@ -0,0 +1,17 @@
+//
+//  bugfinderpetTests.swift
+//  bugfinderpetTests
+//
+//  Created by Yuhao Huang on 6/30/25.
+//
+
+import Testing
+@testable import bugfinderpet
+
+struct bugfinderpetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
